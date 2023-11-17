8325ec085a1853b975608a34bbadb9b6ac8cbf1d
clara0904 <143441412+clara0904@users.noreply.github.com>
2023-11-17 18:55:49 -0300

    Adição de View e Controller

---
+++ BlackJack_MariaClara/src/controller/BlackJackController.java
@@ -0,0 +1,155 @@
+﻿package controller;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import exceptions.ExcecaoPorBaralhoExcedido;
+import exceptions.ExcecaoPorFaltaDeCartasBaralho;
+import models.Baralho;
+import models.Carta;
+import models.Jogador;
+import models.Partida;
+import view.BlackjackView;
+
+public class BlackJackController {
+    Baralho baralho = new Baralho();;
+    BlackjackView blackjackView = new BlackjackView();
+    Scanner scanner = new Scanner(System.in);
+    ArrayList <Jogador> jogadores = new ArrayList<>();
+    Partida informacoesPartida = new Partida();
+    
+    public BlackJackController() throws ExcecaoPorBaralhoExcedido, ExcecaoPorFaltaDeCartasBaralho {
+        baralho.criaBaralho();
+    }
+
+    public void recebeJogadores(){
+        blackjackView.inicioJogo();
+        Jogador jogador = new Jogador(blackjackView.recebeNomeJogador());
+        jogadores.add(jogador);
+        boolean aux = true;
+        while(aux){
+            Jogador novoJogador = new Jogador(blackjackView.recebeNomeJogador());
+            jogadores.add(novoJogador);
+
+            String x = blackjackView.recebeOpcaoAdicaoJogador();
+            if(x.equalsIgnoreCase("S")){
+                aux = true;
+            }else if (x.equalsIgnoreCase("N")){
+                aux = false;
+            }else{
+                blackjackView.opcaoInvalida();
+                break;
+            }
+        }
+    }
+
+    int contaPartidas = 0,i=0;
+    boolean partidaEmAndamento = true;
+    public void inicioJogo() throws ExcecaoPorBaralhoExcedido, ExcecaoPorFaltaDeCartasBaralho{
+        while(partidaEmAndamento){
+            baralho.reiniciaCartasJogadas();
+            String cartasDoJogador = blackjackView.retornaCartas(jogadores.get(i).nomeJogador);
+            blackjackView.imprimeMensagem(cartasDoJogador);    
+            Carta carta = baralho.retornaCarta();   baralho.addCartasJogadas(carta);    baralho.removeCartaBaralho(carta);
+            carta = baralho.retornaCarta();  baralho.addCartasJogadas(carta);   baralho.removeCartaBaralho(carta);
+
+            blackjackView.quebraDeLinha();
+            baralho.imprimeCartasJogadas();
+
+            int somaCartas = baralho.somaCartas();
+            blackjackView.retornaSoma(somaCartas);
+            jogadores.get(i).pontuacao = baralho.somaCartas(); 
+            
+            blackjackView.quebraDeLinha();
+            int opcao = blackjackView.recebeOpcaoJogo();
+            boolean hit = false;
+            if(opcao == 1){
+                hit = true;
+            }else if(opcao == 2){
+                hit = false;
+            }else{
+                blackjackView.opcaoInvalida();
+            }
+            while(hit){
+                carta = baralho.retornaCarta();     baralho.addCartasJogadas(carta);    baralho.removeCartaBaralho(carta);
+                blackjackView.quebraDeLinha();
+                baralho.imprimeCartasJogadas();
+                somaCartas = baralho.somaCartas();
+                blackjackView.retornaSoma(somaCartas);
+                jogadores.get(i).pontuacao = baralho.somaCartas();
+
+                if(baralho.testVitoriaUmVencedor()){
+                    jogadores.get(i).vencedor = true;
+                    blackjackView.vencedorPorBlackjack(jogadores.get(i).nomeJogador);
+                    hit = false;
+                }else if(baralho.testEstouro()){
+                    jogadores.get(i).vencedor = false;
+                    blackjackView.estouro();
+                    hit = false;
+                }else{
+                    blackjackView.quebraDeLinha();
+                    opcao = blackjackView.recebeOpcaoJogo();
+                    if(opcao==1){
+                        hit=true;
+                    } else if (opcao == 2) {
+                        break;
+                    }else{
+                        blackjackView.opcaoInvalida();
+                    }
+                }
+            }
+            i++;
+            String resultadoPartida = null;
+            if(i>=jogadores.size()){ 
+                boolean teveBlackJack = false;
+                for (Jogador procuraVencedor : jogadores) {
+                    if(procuraVencedor.vencedor == true){   //busca por jogador único que fez blackjack
+                        if(baralho.testVitoriaMaisDeUmVencedor(jogadores)){ //busca por mais de um jogador que fez blackjack
+                            for (Jogador procuraVencedores : jogadores) {
+                                resultadoPartida = "Jogador vencedor: "+procuraVencedores.nomeJogador;
+                            }
+                        break;
+                        }
+                        resultadoPartida = "Jogador vencedor: "+procuraVencedor.nomeJogador;
+                        teveBlackJack = true;
+                    }
+                }
+                if(teveBlackJack){
+                    blackjackView.imprimeMensagem(resultadoPartida+"\n");
+                }else{  //se nenhum fez blackjack, busca por empate ou vitoria por proximidade
+                    if(baralho.testEmpateMaiorQue21(jogadores) || baralho.testEmpateIgualBlackJack(jogadores) || baralho.testEmpateMenorQue21(jogadores)){ //empate
+                        resultadoPartida = "Partida empatadada!";
+                        blackjackView.imprimeMensagem(resultadoPartida+"\n");    
+                    }else if(baralho.testSemVitoriaSemEmpate(jogadores)){   //vitoria por proximidade de 21
+                        for (Jogador jogadorVitoria : jogadores) {
+                            if(jogadorVitoria.vencedor == true){
+                                resultadoPartida = "Jogador vencedor: "+jogadorVitoria.nomeJogador;
+                                blackjackView.imprimeMensagem(resultadoPartida+"\n");
+                                break;
+                            }
+                        }
+                    }
+                }
+
+
+                informacoesPartida.addPartida(contaPartidas, jogadores);    //adiciono as informações da partida
+                opcao = blackjackView.recebeOpcaoNovaPartida();
+                if(opcao == 1) {
+                    contaPartidas++;
+                    baralho.reiniciaBaralho();  //baralho inteiro novamente
+                    informacoesPartida.zeraResultadoJogadorPorPartida(jogadores);   //vencedor = false
+                    i=0;
+                    partidaEmAndamento=true;
+                } else if (opcao == 2) {
+                    informacoesPartida.imprimeInformacoesPartidas();    //imprime todas as informações de todas as partidas
+                    partidaEmAndamento = false; 
+                }else{
+                    blackjackView.opcaoInvalida();
+                    break;
+                }
+            }
+        }
+    }
+}
+
+
---
+++ BlackJack_MariaClara/src/view/BlackjackView.java
@@ -0,0 +1,72 @@
+﻿package view;
+import java.util.Scanner;
+
+public class BlackjackView {
+    private Scanner scanner;
+
+    public BlackjackView() {
+        scanner = new Scanner(System.in);
+    }
+
+    public void inicioJogo(){
+        System.out.println("-------------------------");
+        System.out.print("Bem vindo ao BlackJack21!\n");
+        System.out.println("-------------------------");
+    }
+
+    public String recebeNomeJogador(){
+        System.out.print("Digite o nome do jogador: ");
+        return scanner.next();
+    }
+
+    public String recebeOpcaoAdicaoJogador(){
+        System.out.print("Deseja adicionar mais jogadores (S) (N): ");
+        return scanner.next();
+    }
+
+    public int recebeOpcaoJogo(){
+        System.out.print("Deseja 1- Receber mais uma carta 2- Parar: ");
+        return scanner.nextInt();
+    }
+
+    public void opcaoInvalida(){
+        System.out.println("Opcao Invalida!\n");
+    }
+
+    public String retornaCartas(String nomeJogador){
+        return "\nCartas do Jogador: "+nomeJogador;
+    }
+
+    public void retornaSoma(int soma){
+        System.out.println("Soma das cartas: "+soma);
+    }
+
+    public void vencedorPorBlackjack(String nomeJogador){
+        System.out.println("\nBlackJack! Vencedor "+nomeJogador);
+    }
+
+    public void estouro(){
+        System.out.println("\nEstouro!\n");
+    }
+
+    public void verificaVencedor(){
+        System.out.println("Jogador Vencedor: ");
+    }
+
+    public void verificaEmpate(){
+        System.out.println("Partida Empatada!\n");
+    }
+
+    public int recebeOpcaoNovaPartida(){
+        System.out.print("\nDeseja 1- Jogar nova partida 2- Parar: ");
+        return scanner.nextInt();
+    }
+
+    public void quebraDeLinha(){
+        System.out.println("\n");
+    }
+
+    public void imprimeMensagem(String mensagem) {
+        System.out.print(mensagem);
+    }
+}
